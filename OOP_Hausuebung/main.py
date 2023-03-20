e9768daff74b1ec1182f2f6367665d1a2db261f0
Marcel-Fill <marcfill@tsn.at>
2023-03-20 16:44:44 +0100

    .

--- OOP_Hausuebung/main.py
+++ OOP_Hausuebung/main.py
@@ -6,17 +6,23 @@ def createComp(name):
 def createWorker(rang,name,abt,gender,f):
     e = None 
     match rang:
-        case rang.Rang.mitarbeiter:
+        case rang.rang.mitarbeiter:
             e = mitarbeiter.mitarbeiter(name,abt,gender)
-        case rang.Rang.abteilungsLeiter:
+        case rang.rang.abteilungsLeiter:
             e = abteilungsLeiter.abteilungsLeiter(name,abt,gender)
     f.addPerson(e)
     return e
 
 def runCompany():
     f = createComp("YA YEET GmbH UND COKG")
-    m1 = createWorker(rang.Rang.mitarbeiter,"JUSTUS","BWL",geschlecht.geschlecht.undefined,f)
+    m1 = createWorker(rang.rang.mitarbeiter,"JUSTUS","BWL",geschlecht.geschlecht.maennlich,f)
+    setattr(m1,"name","VALLAHBILLA")
+    print(getattr(m1,"abt"))
+    print(m1)
     
+
+    
+
 
 if __name__ == "__main__": 
     runCompany()
