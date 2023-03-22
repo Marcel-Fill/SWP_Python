6d6bb2470c230c5c45dc49aed8793b22807b758b
Marcel-Fill <91134255+Marcel-Fill@users.noreply.github.com>
2023-03-22 12:02:20 +0100

    .

---
+++ observer/weather.java
@@ -0,0 +1,13 @@
+package observer;
+
+class Weather {
+    private String weatherData;
+
+    public Weather(String data){
+        this.weatherData = data;
+    }
+
+    public String getWeatherData(){
+        return this.weatherData;
+    }
+}
