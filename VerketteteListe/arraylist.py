29ae30704cd4f07f43d3007e82c00a170fe68917
Marcel-Fill <91134255+Marcel-Fill@users.noreply.github.com>
2023-03-29 07:45:49 +0200

    .

--- VerketteteListe/arraylist.py
+++ VerketteteListe/arraylist.py
@@ -1,51 +1,51 @@
 class ArrayList():
     def __init__(self):
-        self.arr = [None] * 1
+        self.ArrayList = [None] * 1
         self.size = 1
 
     def append(self, data):
         def append_wrapper():
             for i in range(0, self.size):
                 l = len(self)
-                if self.arr[i] == None:
-                    self.arr[i] = data
+                if self.ArrayList[i] == None:
+                    self.ArrayList[i] = data
                     return True
             return False
         
         res = append_wrapper()
         if res == False:
-            self.arr = self.arr + ([None]*self.size)
+            self.ArrayList = self.ArrayList + ([None]*self.size)
             self.size *= 2
             self.append(data)
         return
     
     def removeIndex(self, index):
         def remove_wrapper():
-            self.arr[index] = None
+            self.ArrayList[index] = None
             if len(self) <= self.size/2:
                 return False
             return True
         res = remove_wrapper()
         if res == False:
-            self.arr = list(filter(lambda x: x != None, self.arr))
+            self.ArrayList = list(filter(lambda x: x != None, self.ArrayList))
             self.size /= 2
             
     def remove(self, data):
         def remove_wrapper():
             for i in range(len(self)-1):
-                if self.arr[i] == data:
+                if self.ArrayList[i] == data:
                     self.removeIndex(i)
             if len(self) <= self.size/2:
                 return False
             return True
         res = remove_wrapper()
         if res == False:
-            self.arr = list(filter(lambda x: x != None, self.arr))
+            self.ArrayList = list(filter(lambda x: x != None, self.ArrayList))
             self.size /= 2
                 
     def __len__(self):
         l = 0
-        for i in self.arr:
+        for i in self.ArrayList:
             if i == None:
                 pass
             else:
@@ -54,7 +54,7 @@ class ArrayList():
 
     def __str__(self):
         info = "["
-        for i in self.arr:
+        for i in self.ArrayList:
             if i != None:
                 info += (str(i) + ",")
         info = (info[:-1] if len(info) > 1 else info) + "]"
@@ -66,12 +66,3 @@ if __name__ == "__main__":
 
     for i in range(9):
         arraylist.append(i)
-
-    print("arraylist")
-    print(arraylist)
-    print("len: ",len(arraylist))
-    print("size: ",arraylist.size)
-    print("remove")
-    arraylist.remove(7)
-    print(arraylist)
-    print("size: ",arraylist.size)
